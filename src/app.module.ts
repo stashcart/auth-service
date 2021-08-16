9390b9408aefb4437806c31c1bd42c117d2ffab6
vladhuk <vladhookovskiy@gmail.com>
2021-08-17 02:24:41 +0300

    - set up rabbitmq;

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    RabbitMQModule.forRootAsync(RabbitMQModule, {
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('RABBITMQ_URL', 'amqp://localhost:5672'),
+        exchanges: [
+          {
+            name: 'user',
+            type: 'topic',
+          },
+        ],
+        connectionInitOptions: { wait: false },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get<number>('HTTP_PORT', 3001));
 }
 
 bootstrap();
