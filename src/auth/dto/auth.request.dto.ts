c5706527a5ecc80a3251b281b9e8bdc88ee14ebe
vladhuk <vladhookovskiy@gmail.com>
2021-08-23 14:35:27 +0300

    - add exmaple values to swagger;

--- src/auth/dto/auth.request.dto.ts
+++ src/auth/dto/auth.request.dto.ts
@@ -1,10 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class AuthRequestDto {
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({
+    example: `user-${Math.floor(Math.random() * 10000)}@gmail.com`,
+  })
   email: string;
 
   @IsNotEmpty()
+  @ApiProperty({ example: 'user' })
   password: string;
 }
