164dc56396e245f5f11aa301e0a8ebe2b29c8d1c
vladhuk <vladhookovskiy@gmail.com>
2021-09-04 01:18:19 +0300

    - add validation decorator;

--- src/auth/dto/auth.request.dto.ts
+++ src/auth/dto/auth.request.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class AuthRequestDto {
   @IsEmail()
@@ -10,6 +10,7 @@ export class AuthRequestDto {
   email: string;
 
   @IsNotEmpty()
+  @IsString()
   @ApiProperty({ example: 'user' })
   password: string;
 }
